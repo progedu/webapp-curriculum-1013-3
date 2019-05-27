ef2f96e1569d2b5464179b1668f151588235c1e1
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2019-05-28 00:09:44 +0900

    ワーシャルフロイド法実装

--- src/main/scala-2.11/ShortestPath.scala
+++ src/main/scala-2.11/ShortestPath.scala
@@ -1,10 +1,6 @@
-/**
-  * Created by soichiro_yoshimura on 2016/06/27.
-  */
 case class Edge(from: Char, to: Char, distance: Int)
 
 object ShortestPath {
-
   /**
     * 頂点
     */
@@ -63,7 +59,7 @@ object ShortestPath {
     while (isUpdated) {
       isUpdated = false
       edges.foreach { e =>
-        if(distances(e.from) != Int.MaxValue
+        if (distances(e.from) != Int.MaxValue
           && distances(e.to) > distances(e.from) + e.distance) {
           distances = distances + (e.to -> (distances(e.from) + e.distance))
           isUpdated = true
@@ -79,14 +75,12 @@ object ShortestPath {
     // 各頂点までの距離の初期化
     var distances = vertexes.map(v => (v -> Int.MaxValue)).toMap
     distances = distances + (start -> 0)
-
     var usedEdges: Set[Edge] = Set()
     var isUpdated = true
-
     while (isUpdated) {
       isUpdated = false
       edges.foreach { e =>
-        if(!usedEdges.contains(e)
+        if (!usedEdges.contains(e)
           && distances(e.from) != Int.MaxValue
           && distances(e.to) > distances(e.from) + e.distance) {
           distances = distances + (e.to -> (distances(e.from) + e.distance))
@@ -95,13 +89,20 @@ object ShortestPath {
         }
       }
     }
-
     println(distances)
     println(distances(goal))
   }
 
   def solveByWarshallFloyd(start: Char, goal: Char): Unit = {
-    ???
+    // 距離の初期化
+    var distanceMap: Map[(Char, Char), Int] = vertexes.map(v => ((v, v) -> 0)).toMap
+    distanceMap = distanceMap ++ edges.map(e => (e.from, e.to) -> e.distance)
+
+    def distance(v1: Char, v2: Char): Int = distanceMap.getOrElse((v1, v2), Int.MaxValue / 2)
+
+    for (v1 <- vertexes; v2 <- vertexes; v3 <- vertexes) {
+      distanceMap = distanceMap + ((v2, v3) -> math.min(distance(v2, v3), distance(v2, v1) + distance(v1, v3)))
+    }
   }
 
 }
